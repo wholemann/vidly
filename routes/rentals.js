95c23cb3a288b774eb996871eb10aec765434610
wholemann <wholeman.dev@gmail.com>
2019-05-22 15:44:05 +0900

    Add transactions logic to rentals API

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,8 +2,11 @@ const {Rental, validate} = require('../models/rental');
 const {Movie} = require('../models/movie'); 
 const {Customer} = require('../models/customer'); 
 const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.use((req, res, next) => {
   console.log('Request URL:', req.originalUrl);
@@ -49,12 +52,24 @@ router.post('/', async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
+  // rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
-  
-  res.send(rental);
+  // movie.numberInStock--;
+  // movie.save();
+
+  try {
+    new Fawn.Task()
+      .save('rentals', rental)
+      .update('movies', { _id: movie._id }, {
+        $inc: { numberInStock: -1 }
+      })
+      .run();
+
+    res.send(rental);
+  }
+  catch (ex) {
+    res.status(500).send('Something failed.');
+  }
 });
 
 router.get('/:id', async (req, res) => {
