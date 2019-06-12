f88c453864325281283758f338cfa2c5bc409b4f
wholemann <wholeman.dev@gmail.com>
2019-06-12 15:36:02 +0900

    Refactor project structure

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
-// process.on('uncaughtException', (ex) => {
-//   winston.error(ex.message);
-//   process.exit(1);
-// });
-
-// const p = Promise.reject(new Error('Something failed miserably!'));
-// p.then(() => console.log('Done'));
-
-// throw new Error('Something failed duraing startup.');
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+module.exports = server;
